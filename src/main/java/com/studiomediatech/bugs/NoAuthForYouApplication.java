c85eec6ff1550aaefc4d7954ad9a3fd68d71d6e4
Olle Törnström <olle@studiomediatech.com>
2016-04-20 20:45:40 +0200

    Initial Spring Boot starter

---
+++ src/main/java/com/studiomediatech/bugs/NoAuthForYouApplication.java
@@ -0,0 +1,12 @@
+package com.studiomediatech.bugs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoAuthForYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoAuthForYouApplication.class, args);
+	}
+}
