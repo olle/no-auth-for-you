0021012bf004bd4fbd6c4280c9e411097d420edd
Olle Törnström <olle@studiomediatech.com>
2016-04-22 17:12:39 +0200

    Add note on actuator problem
    * So, it looks like the culprit is actuator! Well, well, well...
      Mr. Anderson, it seems you've been living.... two lives.

---
+++ spring-boot-1.3.x/README.md
@@ -0,0 +1,4 @@
+# NOTE:
+
+> So, in this setup the authentication is only missing when I include the
+  `spring-boot-starter-actuator` - ha, go figure!
--- spring-boot-1.3.x/src/main/java/com/studiomediatech/bugs/NoAuthForYouApplication.java
+++ spring-boot-1.3.x/src/main/java/com/studiomediatech/bugs/NoAuthForYouApplication.java
@@ -2,53 +2,62 @@ package com.studiomediatech.bugs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.SearchStrategy;
-import org.springframework.boot.autoconfigure.web.DefaultErrorAttributes;
-import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+
 
 @SpringBootApplication
 public class NoAuthForYouApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NoAuthForYouApplication.class, args);
-	}
+    public static void main(String[] args) {
 
-	@Configuration
-	public static class WebConfig extends WebMvcAutoConfigurationAdapter {
+        SpringApplication.run(NoAuthForYouApplication.class, args);
+    }
 
-		@Override
-		public void addInterceptors(InterceptorRegistry registry) {
+    @Configuration
+    public static class WebConfig extends WebMvcAutoConfigurationAdapter {
 
-			registry.addInterceptor(new LoggingHandlerInterceptor()).addPathPatterns("/**");
-		}
-	}
+        @Override
+        public void addInterceptors(InterceptorRegistry registry) {
 
-	@Configuration
-	@EnableWebSecurity
-	@EnableGlobalMethodSecurity(prePostEnabled = true)
-	public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+            registry.addInterceptor(new LoggingHandlerInterceptor()).addPathPatterns("/**");
+        }
+    }
 
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.authorizeRequests().antMatchers("/**").authenticated();
-			http.formLogin().permitAll();
-			http.csrf().disable();
-		}
+    @Configuration
+    @EnableWebSecurity
+    @EnableGlobalMethodSecurity(prePostEnabled = true)
+    public static class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-		@Override
-		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-			auth.inMemoryAuthentication().withUser("user").password("user").roles("USER").and().withUser("admin")
-					.password("admin").roles("ADMIN", "USER");
-		}
-	}
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+
+            http.authorizeRequests().antMatchers("/**").authenticated();
+            http.formLogin().permitAll();
+            http.csrf().disable();
+        }
+
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+            auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("user")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN", "USER");
+        }
+    }
 }
