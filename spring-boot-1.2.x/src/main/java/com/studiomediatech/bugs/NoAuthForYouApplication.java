357b1bec7ad0cde373c21bac54560cc3d6b14d5e
Olle Törnström <olle@studiomediatech.com>
2016-04-22 17:14:37 +0200

    Adapt to reverse profile switch

--- spring-boot-1.2.x/src/main/java/com/studiomediatech/bugs/NoAuthForYouApplication.java
+++ spring-boot-1.2.x/src/main/java/com/studiomediatech/bugs/NoAuthForYouApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.context.embedded.FilterRegistrationBean;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -35,6 +36,7 @@ public class NoAuthForYouApplication {
 
 
     @Bean
+    @Profile("!without-fix")
     public FilterRegistrationBean getSpringSecurityFilterChainBindedToError(
         @Qualifier("springSecurityFilterChain") Filter springSecurityFilterChain) {
 
